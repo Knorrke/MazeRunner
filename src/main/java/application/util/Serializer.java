64b58ba47a1706fc405325654bd344d428e4be1e
Knorrke <knorr.b@gmx.de>
2018-05-22 18:39:09 +0200

    refactor: remove empty constructor

--- src/main/java/application/util/Serializer.java
+++ src/main/java/application/util/Serializer.java
@@ -3,8 +3,6 @@ package application.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class Serializer {
-  private Serializer() {}
-
   public static ObjectMapper create() {
     ObjectMapper objectMapper = new ObjectMapper();
     return objectMapper;
